f3aff6c8787c1c0dd57dcaebe92b75dcac7bd3d8
Smerom <Smerom@users.noreply.github.com>
2018-06-10 15:29:56 -0700

    travis setup

---
+++ get-deps.sh
@@ -0,0 +1,18 @@
+cd ~/
+sudo apt-get update
+sudo apt-get install autoconf automake libtool curl make g++ unzip
+sudo apt-get install build-essential pkg-config
+curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.tar.gz
+tar -xzf protobuf-cpp-3.5.1.tar.gz
+cd protobuf-cpp-3.5.1
+./configure
+make
+make check
+sudo make install
+sudo ldconfig # refresh shared library cache.
+
+git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+make
+sudo make install
