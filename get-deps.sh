72359c628294a8fbbe128b0b92ec995ff2b1f8b8
qm <1271721+Smerom@users.noreply.github.com>
2018-12-06 14:52:33 -0800

    switch to circle ci

--- get-deps.sh
+++ get-deps.sh
@@ -2,7 +2,7 @@ cd ~/
 sudo apt-get update
 sudo apt-get install autoconf automake libtool curl make g++ unzip
 sudo apt-get install build-essential pkg-config
-curl -OL https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz
 tar -xzf protobuf-cpp-3.6.1.tar.gz
 cd protobuf-3.6.1
 ./configure
